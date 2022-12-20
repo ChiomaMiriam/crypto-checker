c1e5d06135b86365373bcd6165c912110dfd25e5
ChiomaMiriam <mijegchioma@gmail.com>
2022-12-20 14:03:07 +0100

    cryptochecker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { CurrencyService } from './service/currency.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'crypto-checker';
+  selectedCurrency: string = 'NGN';
+  constructor(private currencyService: CurrencyService) {}
+  sendCurrency(event: string) {
+    console.log(event);
+    this.currencyService.setCurrency(event);
+  }
 }
